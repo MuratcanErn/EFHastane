ffbab60f91ffdbc2a3e98bc27221bfe5e5676ff5
Muratcan <hmuratcaneren@hotmail.com>
2024-02-14 01:26:19 +0300

    EFHastane

---
+++ EFHospital_0/EFHospital_0/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace EFHospital_0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHasta = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstHastalar = new System.Windows.Forms.ListBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.cmbDoktor = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txtHasta
+            // 
+            this.txtHasta.Location = new System.Drawing.Point(86, 52);
+            this.txtHasta.Name = "txtHasta";
+            this.txtHasta.Size = new System.Drawing.Size(100, 20);
+            this.txtHasta.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hasta Adı:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Doktor Adı:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lstHastalar
+            // 
+            this.lstHastalar.FormattingEnabled = true;
+            this.lstHastalar.Location = new System.Drawing.Point(264, 29);
+            this.lstHastalar.Name = "lstHastalar";
+            this.lstHastalar.Size = new System.Drawing.Size(401, 303);
+            this.lstHastalar.TabIndex = 2;
+            this.lstHastalar.Click += new System.EventHandler(this.lstHastalar_Click);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(61, 213);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(89, 21);
+            this.btnEkle.TabIndex = 3;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(61, 251);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(89, 21);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(61, 289);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(89, 21);
+            this.btnGuncelle.TabIndex = 3;
+            this.btnGuncelle.Text = "Guncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // cmbDoktor
+            // 
+            this.cmbDoktor.FormattingEnabled = true;
+            this.cmbDoktor.Location = new System.Drawing.Point(90, 130);
+            this.cmbDoktor.Name = "cmbDoktor";
+            this.cmbDoktor.Size = new System.Drawing.Size(121, 21);
+            this.cmbDoktor.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbDoktor);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.lstHastalar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtHasta);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHasta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstHastalar;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.ComboBox cmbDoktor;
+    }
+}
+
---
+++ EFHospital_0/EFHospital_0/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EFHospital_0.Models;
+
+namespace EFHospital_0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            _db= new HastaneDBEntities();
+        
+        }
+       HastaneDBEntities _db;
+
+        public void Ekle()
+        {
+            lstHastalar.DataSource = _db.Hastalars.ToList();
+            lstHastalar.DisplayMember = "Isim";
+            lstHastalar.SelectedIndex = -1;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Ekle();
+            cmbDoktor.DataSource=_db.Doktorlars.ToList();
+            cmbDoktor.DisplayMember = "Isim";
+            cmbDoktor.SelectedIndex = -1;
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            Hastalar h=new Hastalar();
+            h.Isim=txtHasta.Text;
+            h.Doktorlar = cmbDoktor.SelectedItem as Doktorlar;
+            _db.Hastalars.Add(h);
+            _db.SaveChanges();
+            Ekle();                
+           
+        }
+        Hastalar hasta;
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            if (lstHastalar.SelectedIndex >-1)
+            {
+                _db.Hastalars.Remove(hasta);
+                _db.SaveChanges();
+                Ekle();
+                hasta = null;
+            }
+        }
+
+        private void lstHastalar_Click(object sender, EventArgs e)
+        {
+            if(lstHastalar.SelectedIndex > -1)
+            {
+                hasta=lstHastalar.SelectedItem as Hastalar;
+            }
+
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            if (hasta!=null)
+            {
+                hasta.Isim = txtHasta.Text;
+                hasta.Doktorlar = cmbDoktor.SelectedItem as Doktorlar;
+                _db.SaveChanges();
+                Ekle();
+                hasta = null;
+            }
+        }
+    }
+}
+
